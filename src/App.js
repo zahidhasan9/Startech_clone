8883fc1a188f7ab752f654bca46bafb18af029b4
jahidhasanrimel <zmzahidhasan181@gmail.com.com>
2023-11-30 23:16:50 +0600

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Cart_page from "./Compoments/Cart_page";
 // import './App.css';
 import Footer from "./Compoments/Footer";
@@ -10,17 +10,17 @@ import Navbar from './Compoments/Navbar';
 import Navbar_copy from './Compoments/Navbar copy';
 function App() {
   return (
-  <>
-  <div className="bg-gray content ">
-   <Navbar_copy/>
-  {/* <Navbar/> */}
-  <Cart_page/>
-  <Banner/>
-<Middle_Page_main/>
-  <About/>
-  <Footer/>
-  </div>
-  </>
+    <>
+      <div className="bg-gray content ">
+        <Navbar_copy />
+        {/* <Navbar/> */}
+        <Cart_page />
+        <Banner />
+        <Middle_Page_main />
+        <About />
+        <Footer />
+      </div>
+    </>
 
   );
 }
